84d287e44e1994871dcf1776f081e336ab660594
NJ <noeljustine.mendoza@gmail.com>
2022-08-30 18:18:54 +0800

    Offline

---
+++ app/src/main/java/com/cubbysulotions/proo/ModelsClasses/FirebaseOffline.java
@@ -0,0 +1,13 @@
+package com.cubbysulotions.proo.ModelsClasses;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseOffline extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
